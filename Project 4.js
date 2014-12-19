e5032ac77105e09a4c54991694c39bf726f45da9
Dennis Burns <daburns@fullsail.edu>
2014-12-18 19:43:49 -0500

    added array
    arrays

--- Project 4.js
+++ Project 4.js
@@ -51,3 +51,74 @@ function isEmail(str) {
   }
   return false;
 }
+
+//URL String 
+function isURL(str) {
+  return (str.indexOf('http://') === 0) || (str.indexOf('https://') === 0);
+}
+//Upper and Lowercase string 
+function titleCase(str) {
+  return Array.prototype.map.call(str, function(val, idx, arr) {
+    // Uppercase first letter of string.
+    if(idx === 0) {
+      return val.toUpperCase();
+    }
+
+    // Uppercase letter after space.
+    if(arr.charAt(idx - 1) === ' ') {
+      return val.toUpperCase();
+    }
+
+    // Lowercase anything else.
+    return val.toLowerCase();
+  }).join('');
+}
+//Seperate string
+function replaceSeperator(str, seperator, newSeperator) {
+  return str.replace(seperator, newSeperator);
+}
+
+// Number
+// Format a number to use a specific number of decimal places
+function formatDecimal(num, decimals) {
+  return num.toFixed(decimals);
+}
+//Fuzzy-match a number
+function fuzzyMatch(num, compare, fuzziness) {
+  return Math.abs(compare - num) / num < fuzziness;
+}
+
+// Difference in days.
+function diffDates(a, b) {
+  return (a - b) / 1000 / 60 / 60 / 24;
+}
+//Given a string version of a number 
+function strToNum(str) {
+  return parseInt(str, 10);
+}
+// Array
+
+// Find smallest number greater than min, or NaN if all are smaller.
+function minVal(arr, min) {
+  return arr.reduce(function(a, b) {
+    if(isNaN(a) && b > min) return b;
+    else if(b > min) return Math.min(a, b);
+    else if(a > min) return a;
+    return NaN;
+  });
+}
+//Find the total value of just the numbers in an array, even if some of the items are not numbers.
+function total(arr) {
+  return arr.reduce(function(a, b) {
+    if(typeof b === 'number' && !isNaN(b)) {
+      return a + b;
+    }
+    return a;
+  });
+}
+//Given an array of objects and the name of a key, return the array sorted by the value of that key in each of the objects
+function sortKey(arr, key) {
+  return arr.sort(function(a, b) {
+    return a[key] - b[key];
+  });
+}
